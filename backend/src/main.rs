c7ef4aaad674b87d293bc6600d87c23e927ed49c
Ian Luckraft <ian@ianluckraft.co.uk>
2024-02-03 07:37:56 +0000

    Add a `GET` route to output `Hello, World!`

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
